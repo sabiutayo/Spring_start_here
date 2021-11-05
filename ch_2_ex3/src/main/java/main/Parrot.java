d8f65f7eefcd8b699a1529881d371692fde6ed96
sabiutayo <sabiutayo@yahooo.com>
2021-11-05 13:50:58 +0200

    init

---
+++ ch_2_ex3/src/main/java/main/Parrot.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Parrot {
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Parrot setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
---
+++ ch_2_ex4/src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot p = context.getBean("miki",Parrot.class);
+        System.out.println(p.getName());
+    }
+}
