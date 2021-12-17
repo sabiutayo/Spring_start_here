c5efb463a37c218107f1111f658a018df4ab83df
Sabie George Ionut <sabiutayo@yahoo.com>
2021-12-17 19:32:42 +0200

    clean

---
+++ sq_ch8_ex2/src/test/java/com/spring_start_here/sq_ch8_ex1/MainTests.java
@@ -0,0 +1,13 @@
+package com.spring_start_here.sq_ch8_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
