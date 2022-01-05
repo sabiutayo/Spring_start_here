7a5404df6cb55f9f9ceec118670e1e7b330a22e7
sabiutayo <sabiutayo@yahooo.com>
2022-01-05 18:36:42 +0200

    sq_ch12_ex2

---
+++ sq_ch12_ex2/src/test/java/spring/start/here/sq_ch12_ex2/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.start.here.sq_ch12_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
